1337cd7ad5f6de3e1b9c85bce5e801eb012f52aa
dongxu.chen <nx-engine2018>
2019-09-20 09:30:17 +0800

    dev测试修改

--- src/main/java/com/example/demp/controller/FirstController.java
+++ src/main/java/com/example/demp/controller/FirstController.java
@@ -14,7 +14,7 @@ public class FirstController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return "hello Git666";
+        return "hello Git888";
     }
 }
 
